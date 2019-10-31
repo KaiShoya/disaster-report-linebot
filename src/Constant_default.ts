237c023e0a58907130403b3c4a1c6e5c189dbf6e
Kai Shoya <s-kai@lumber-mill.co.jp>
2019-10-31 19:07:10 +0900

    間違ってコミットしたため戻した

--- src/Constant_default.ts
+++ src/Constant_default.ts
@@ -1,8 +1,9 @@
-// // Messaging APIアクセストークン
-// const CHANNEL_ACCESS_TOKEN = '';
-// // 管理ユーザーID
-// const ADMINID = '';
-// // スプレッドシート情報
-// const SHEET_ID = '1TZpJN3PiDynf9iaT1dhprwMq9hWnhHBZR0b3uBDdfl0';
-// const SHEET_LOG = 'ログ';
-// const SHEET_DRAFT = '下書き';
+// Messaging APIアクセストークン
+const CHANNEL_ACCESS_TOKEN = ''
+// 管理ユーザーID
+const ADMINID = ''
+// スプレッドシート情報
+const SHEET_ID = '1TZpJN3PiDynf9iaT1dhprwMq9hWnhHBZR0b3uBDdfl0'
+const SHEET_LOG = 'ログ'
+const SHEET_DRAFT = '下書き'
+const SHEET_LOCATION = '災害現場一覧'
